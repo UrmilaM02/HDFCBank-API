5a5842c1d64039ea4181cfcd40b60fb31b5614eb
Ashish Kumar Mishra <ashishmishra102001@gmail.com>
2025-06-07 12:57:29 +0530

    Arrays file add in my project

--- String01.java
+++ String01.java
@@ -1,1 +1,22 @@
-Arrays file add in bank project
+
+import java.util.*;
+public class Matrics {
+
+    public static void main(String[] args) {
+        int matrix [][]= new int [3][3];
+        int n=3;
+        int m=3;
+
+
+        Scanner sc= new Scanner(System.in);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+    }
+}
+
+
+
